32549a4f5c9b8892a08b107085f0f8d31f7caa0d
Maryen <voujum@gmail.com>
2023-01-31 19:43:09 +0100

    Take photo feature

--- wb/19-webcam/app.js
+++ wb/19-webcam/app.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.taken-photos-container');
 const snap = document.querySelector('.snap');
 
-// Pipe video into <video class="player"> element
+// Pipe webcam video into video element
 function getWebcamVideo() {
   
   // >>navigator<< 
@@ -38,7 +38,7 @@ function getWebcamVideo() {
     }); 
 }
 
-function webcamVideoToCanvas() {
+function videoToCanvas() {
   // Adjusting canvas size to video size
   const width = canvas.width = video.videoWidth;
   const height = canvas.height = video.videoHeight;
@@ -48,11 +48,24 @@ function webcamVideoToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
   }, 17); 
   // could:
-  // test machine performance and adjust accordingly
+  // test machine performance and adjust ms accordingly
   // implement >>requestAnimationFrame<<?
+}
+
+function takePhoto() {
+  // Setting sound at 0 when photo is taken to make sure it plays each time
+  snap.currentTime = 0;
+  snap.play();
+
+  const imageData = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = imageData;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src=${imageData}>`;
+  strip?.prepend(link);
 }
 
 
 getWebcamVideo();
 
-video?.addEventListener('canplay', webcamVideoToCanvas);
+video?.addEventListener('canplay', videoToCanvas);
