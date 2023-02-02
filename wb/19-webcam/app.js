a23778a1f8a864d6a6820ceff8bcfbd2d3d94718
Maryen <voujum@gmail.com>
2023-02-02 17:06:17 +0100

    Added filters

--- wb/19-webcam/app.js
+++ wb/19-webcam/app.js
@@ -4,6 +4,10 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.taken-photos-container');
 const snap = document.querySelector('.snap');
 
+const rgbRange = document.querySelectorAll('.green-screen input');
+const effects = document.querySelectorAll('.effects select');
+const ghost = document.getElementById('ghost');
+
 // Pipe webcam video into video element
 function getWebcamVideo() {
   
@@ -14,7 +18,7 @@ function getWebcamVideo() {
   // >>navigator.mediaDevices<< 
   // read-only prop returns obj, 
   // which provides access to connected media devices like
-  // cameras an microphones, as well as screen sharing
+  // cameras, microphones, as well as screen sharing
 
   // >>MediaDevices.getUserMedia()<<
   // prompts the user for permission to use a media input which produces 
@@ -25,10 +29,9 @@ function getWebcamVideo() {
   // of a display or portion thereof (such as a window) as a MediaStream.
   navigator.mediaDevices.getUserMedia({video: true, audio: false}) 
     .then(stream => {
-
+      video.srcObject = stream;
       // >>srcObject<< 
       // sets obj like MediaStream, MediaSource, Blob, File as src
-      video.srcObject = stream;
 
       // video.play() is not necessary if >>autopllay<< attribute
     })
@@ -46,6 +49,19 @@ function videoToCanvas() {
   // Painting webcam video onto canvas
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // >>CanvasRenderingContext2D.getImageData()<< 
+    // returns an ImageData object representing the underlying pixel data for a specified portion of the canvas. 
+    // Huge integer array, each pixel represented with four entries (value 0 - 255)
+    // 0: red, 1: green, 2: blue, 3: alpha, 4: red, 5: green...
+    // first px..........................., second px..........
+
+    let fn = effects[0].value + 'Effect';
+    if (fn != 'noneEffect') pixels = window[fn](pixels);
+
+    ctx.globalAlpha = ghost.value;
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 17); 
   // could:
   // test machine performance and adjust ms accordingly
@@ -60,9 +76,77 @@ function takePhoto() {
   const imageData = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = imageData;
-  link.setAttribute('download', 'handsome');
+  link.setAttribute('download', 'your-new-image');
   link.innerHTML = `<img src=${imageData}>`;
   strip?.prepend(link);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] += 100;
+    pixels.data[i + 1] -= 50;
+    pixels.data[i + 2] *= 0.5;
+  }
+
+  return pixels;
+}
+
+function greenEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] -= 50;
+    pixels.data[i + 1] += 100;
+    pixels.data[i + 2] *= 0.2;
+  }
+
+  return pixels;
+}
+
+function blueEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] -= 50;
+    pixels.data[i + 1] -= 50;
+    pixels.data[i + 2] += 100;
+  }
+
+  return pixels;
+}
+
+function splitEffect(pixels) {
+  // debugger;
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 50] = pixels.data[i + 0];
+    pixels.data[i + 80] = pixels.data[i + 1];
+    pixels.data[i - 50] = pixels.data[i + 2];
+  }
+
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  let ranges = {};
+
+  rgbRange.forEach(input => {
+    ranges[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    red = pixels.data[i + 0];
+    blue = pixels.data[i + 1];
+    green = pixels.data[i + 2];
+
+    if (red >= ranges.rmin && 
+      green >= ranges.gmin && 
+      blue >= ranges.bmin && 
+      red <= ranges.rmax && 
+      green <= ranges.gmax && 
+      blue <= ranges.bmax) {
+        pixels.data[i + 3] = 0; 
+        // making that pixel transparent
+        // alpha channel = 0
+      }
+  }
+
+  return pixels;
 }
 
 
