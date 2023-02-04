1065fe5d090689a7ab807b69d8c7c7af87811b16
Maryen <voujum@gmail.com>
2023-02-04 14:04:33 +0100

    Added basic tts functionality

--- wb/23-text-to-speech/app.js
+++ wb/23-text-to-speech/app.js
@@ -1,8 +1,46 @@
-const utterance = new SpeechSynthesisUtterance();
-let voices = [];
+const utterThis = new SpeechSynthesisUtterance();
+let voices;
 const voicesDropdown = document.querySelector('[name="voice"]');
-const settings = document.querySelectorAll('[type="range"], [type="text"]');
+const settings = document.querySelectorAll('[type="range"], [name="text"]');
+const text = document.querySelector('textarea');
 
-const readButton = document.getElementById('read');
+const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
-console.log(readButton, stopButton);
+
+utterThis.text = text?.value;
+
+function getVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices.filter(voice => {
+    return voice.name.includes('English');
+  })
+    .map(voice => {
+      return `<option value="${voice.name}">${voice.name}</option>`;
+    }).join('');
+}
+
+function chooseVoice() {
+  utterThis.voice = voices.find(voice => {
+    return voice.name.includes(this.value);
+  });
+
+  speak();
+}
+
+function setValue() {
+  utterThis[this.name] = this.value;
+  speak();
+}
+
+function speak(speak = true) {
+  speechSynthesis.cancel();
+  if (speak) speechSynthesis.speak(utterThis);
+}
+
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+voicesDropdown?.addEventListener('input', chooseVoice);
+
+settings.forEach(setting => setting.addEventListener('change', setValue));
+
+startButton?.addEventListener('click', speak);
+stopButton?.addEventListener('click', speak.bind(null, false));
