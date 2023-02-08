e2a21c12b771bf1538d0c707ca9c3bee71c41072
Maryen <voujum@gmail.com>
2023-02-08 15:51:25 +0100

    Making code cleaner and clearer

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -2,45 +2,54 @@ const tablecells = Array.from(document.querySelectorAll('.tic-tac-toe td'));
 const reset = document.querySelector('.reset button');
 const turn = document.querySelector('.turn span');
 
-let playerOne;
-let playerOneField, playerTwoField;
+let player;
+let playerOneOptionsPlayed, playerTwoOptionsPlayed;
 newGame();
 
 function newGame() {
-  playerOne = true;
+  updateCurrentPlayer(true);
+
   tablecells.forEach(tablecell => tablecell.innerHTML = '');
 
-  function resetField() {
-    return { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0};
-  }
-
-  playerOneField = resetField();
-  playerTwoField = resetField();
+  playerOneOptionsPlayed = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0};
+  playerTwoOptionsPlayed = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0};
 }
 
 function playMove() {
   if (this.firstChild) return;
+  
+  this.insertAdjacentHTML('afterbegin', player == 'X' ? `<span class="x"></span>` : `<span class="o"></span>`);
 
-  this.insertAdjacentHTML('afterbegin', playerOne ? `<span class="x"></span>` : `<span class="circle"></span>`);
-
-  let patternNumbers = this.classList;
-
-  let field = (playerOne) ? playerOneField : playerTwoField;
-
-  for (let number of patternNumbers) {
-    field[number]++;
-    if (field[number] == 3) {
-      gameWon(playerOne);
-      return;
-    }
-  }
-
-  playerOne = !playerOne;
-  turn.innerHTML = (playerOne) ? '✗' : 'O';
+  if (!isWinner(this)) updateCurrentPlayer();
 }
 
-function gameWon(playerOne) {
-  alert((playerOne) ? 'Player X won the game!' : 'Player O won the game!');
+function isWinner(cell) {
+  let patternPlayed = (player == 'X') ? playerOneOptionsPlayed : playerTwoOptionsPlayed;
+  let currentCellPatternNumbers = cell.classList;
+
+  for (let number of currentCellPatternNumbers) {
+    patternPlayed[number]++;
+    
+    if (patternPlayed[number] == 3) {
+      // sheduling gameWon() so browser has a chance to render a move
+      setTimeout(() => gameWon(player), 0);
+      return true;
+    }
+  }
+}
+
+function updateCurrentPlayer(newGame = false) {
+  if (!newGame) {
+    player = (player == 'X') ? "O" : 'X';
+  } else {
+    player = 'X';
+  }
+
+  turn.innerHTML = (player == 'X') ? 'X' : 'O';
+}
+
+function gameWon(player) {
+  alert((player == 'X') ? 'Player X won the game!' : 'Player O won the game!');
   newGame();
 }
 
