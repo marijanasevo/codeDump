15d49d8fd8038b71d083ce48e4d56393c7bdbcee
Maryen <voujum@gmail.com>
2023-02-08 13:39:56 +0100

    First try/version

---
+++ tic-tac-toe/app.js
@@ -0,0 +1,58 @@
+const tablecells = Array.from(document.querySelectorAll('.tic-tac-toe td'));
+const reset = document.querySelector('.reset button');
+
+let playerOne;
+let playerOneField, playerTwoField;
+newGame();
+
+function newGame() {
+  playerOne = true;
+  tablecells.forEach(tablecells => tablecells.innerHTML = '');
+
+  playerOneField = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0
+  };
+  
+  playerTwoField = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0
+  };
+}
+
+function playMove() {
+  if (this.firstChild) return;
+
+  this.insertAdjacentHTML('afterbegin', playerOne ? `<span class="x"></span>` : `<span class="circle"></span>`);
+
+  let patternNumbers = this.classList;
+
+  let field = (playerOne) ? playerOneField : playerTwoField;
+
+  for (let number of patternNumbers) {
+    field[number]++;
+    if (field[number] == 3) gameWon(playerOne);
+  }
+
+  playerOne = !playerOne;
+}
+
+function gameWon(playerOne) {
+  alert((playerOne) ? 'Player X won the game!' : 'Player O won the game!');
+  newGame();
+}
+
+tablecells.forEach(tablecell => tablecell.addEventListener('click', playMove));
+reset.addEventListener('click', newGame);
