3a8cb268f5dfd391fc531fc5003f91cb6a0cc8e3
Maryen <voujum@gmail.com>
2023-02-09 14:56:29 +0100

    Version: Properties hidden using closures and constructor function

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -2,48 +2,49 @@ const tablecells = document.querySelectorAll('.tic-tac-toe td');
 const resetButton = document.querySelector('.reset button');
 const turnContainer = document.querySelector('.turn span');
 
-class TicTacToe {
+function TicTacToe() {
 
-  constructor() {
-    // Events
-    tablecells.forEach(tablecell => tablecell.addEventListener('click', (e) => this.playMove(e)));
-    resetButton.addEventListener('click', this.newGame.bind(this));
+  // Events
+  tablecells.forEach(tablecell => tablecell.addEventListener('click', (e) => playMove(e)));
+  resetButton.addEventListener('click', () => newGame());
 
-    // Start the first game
-    this.newGame();
-  }
+  let player;
+  let optionsPlayed;
 
-  newGame() {
-    this.updateCurrentPlayer(true);
+  // Start the first game
+  newGame();
+
+  function newGame() {
+    updateCurrentPlayer(true);
 
     tablecells.forEach(tablecell => tablecell.innerHTML = '');
 
-    this.optionsPlayed = { 
+    optionsPlayed = { 
       'X': { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0},
       'O': { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
     };
   }
 
-  updateCurrentPlayer(newGame = false) {
+  function updateCurrentPlayer(newGame = false) {
     if (!newGame) {
-      this.player = (this.player == 'X') ? "O" : 'X';
+      player = (player == 'X') ? "O" : 'X';
     } else {
-      this.player = 'X';
+      player = 'X';
     }
   
-    turnContainer.innerHTML = this.player;
+    turnContainer.innerHTML = player;
   }
 
-  playMove(e) {
+  function playMove(e) {
     if (e.currentTarget.firstChild) return;
     
-    e.target.insertAdjacentHTML('afterbegin', this.player == 'X' ? `<span class="x"></span>` : `<span class="o"></span>`);
+    e.target.insertAdjacentHTML('afterbegin', player == 'X' ? `<span class="x"></span>` : `<span class="o"></span>`);
 
-    if (!this.isWinner(e.target)) this.updateCurrentPlayer();
+    if (!isWinner(e.target)) updateCurrentPlayer();
   }
 
-  isWinner(cell) {
-    let patternPlayed = this.optionsPlayed[this.player];
+  function isWinner(cell) {
+    let patternPlayed = optionsPlayed[player];
     let currentCellPatternNumbers = cell.classList;
 
     for (let number of currentCellPatternNumbers) {
@@ -51,15 +52,15 @@ class TicTacToe {
       
       if (patternPlayed[number] == 3) {
         // sheduling gameWon() so browser has a chance to render a move
-        setTimeout(() => this.gameWon(), 0);
+        setTimeout(() => gameWon(), 0);
         return true;
       }
     }
   }
 
-  gameWon() {
-    alert((this.player == 'X') ? 'Player X won the game!' : 'Player O won the game!');
-    this.newGame();
+  function gameWon() {
+    alert((player == 'X') ? 'Player X won the game!' : 'Player O won the game!');
+    newGame();
   }
 }
 
